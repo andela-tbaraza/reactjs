0023359f3f47630ea071eacb20c2c60608f2cc84
Tonida Baraza <tonida.baraza@andela.com>
2016-08-04 01:27:05 +0300

    Ready for react armed to teeth

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const Main = () => {
-  return (
-    <h1>Hello worlds</h1>
-  );
-};
+class Main extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: 'single'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      data: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.data}</h1>
+        <input onChange={this.handleChange} />
+      </div>
+    );
+  }
+}
 
 export default Main;
